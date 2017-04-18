14bf555f0288a2681f98fae0e4f84a7bd9f237ed
pablofleonhart <pablo.fl11@gmail.com>
2017-04-17 23:11:48 -0300

    Structured in a class 'Heap'

--- dheap.cpp
+++ dheap.cpp
@@ -5,145 +5,164 @@
 
 using namespace std;
 
-void heapifyDown( vector<int> &nodes, int len, int index, int k )
+class Heap
+{
+    int n, k;
+    vector<int> weights;
+    vector<int> vertexes;
+    set< pair<int, int> > idx;
+
+public:
+    Heap( int k );
+
+    void printWeights();
+
+    void insert( int weight, int vertex );
+
+    pair<int, int> deleteMin();
+
+    void heapifyDown( int index );
+
+    void heapifyUp( int index );
+};
+
+Heap::Heap( int k )
+{
+	n = 0;
+	this->k = k;
+}
+
+void Heap::printWeights()
+{
+    cout << "Built Heap:" << endl;
+    for ( int i = 0; i < n; i++ )
+        cout << weights[ i ] << "(" << vertexes[ i ] << ") ";
+
+    cout << endl;
+}
+
+void Heap::heapifyDown( int index )
 {
 	vector<int> child;
 
 	child.push_back( 0 );
 	for ( int i = 1; i <= k; i++ )
 	{
-		child.push_back( ( ( k * index + i ) < len ) ? ( k * index + i ) : -1 );
+		child.push_back( ( ( k * index + i ) < n ) ? ( k * index + i ) : -1 );
 	}
 
 	int min_child = -1, min_child_index;
 
 	for ( int i = 1; i <= k; i++ )
 	{
-		if ( child[ i ] != -1 && ( nodes[ child[ i ] ] < min_child || min_child == -1 ) )
+		if ( child[ i ] != -1 && ( weights[ child[ i ] ] < min_child || min_child == -1 ) )
 		{
 			min_child_index = child[ i ];
-			min_child = nodes[ child[ i ] ];
+			min_child = weights[ child[ i ] ];
 		}
 	}
 
 	if ( min_child != -1 )
 	{
-		if ( nodes[ index ] > nodes[ min_child_index ] )
+		if ( weights[ index ] > weights[ min_child_index ] )
 		{
-			swap( nodes[ index ], nodes[ min_child_index ] );
+			swap( weights[ index ], weights[ min_child_index ] );
+			swap( vertexes[ index ], vertexes[ min_child_index ] );
 		}
 
 		index = min_child_index;
-		heapifyDown( nodes, len, index, k );
+		heapifyDown( index );
 	}
 }
 
-void heapifyUp( vector<int> &nodes, int index, int k )
+void Heap::heapifyUp( int index )
 {
 	if ( index > 0 )
 	{
 		int parent = floor( ( index - 1 ) / k );
 
-		if ( nodes[ index ] < nodes[ parent ] )
+		if ( weights[ index ] < weights[ parent ] )
 		{
-			swap( nodes[ index ], nodes[ parent ] );
-			heapifyUp( nodes, parent, k );
+			swap( weights[ index ], weights[ parent ] );
+			swap( vertexes[ index ], vertexes[ parent ] );
+			heapifyUp( parent );
 		}
 	}
 }
 
-void insert( vector<int> &nodes, int* n, int k, int elem )
+void Heap::insert( int weight, int vertex )
 {
-	nodes.push_back( elem );
+	weights.push_back( weight );
+	vertexes.push_back( vertex );
 
-	*n = *n + 1;
+	n++;
 
-	heapifyUp( nodes, *n - 1, k );	
+	heapifyUp( n - 1 );	
 }
 
-int deleteMin( vector<int> &nodes, int* n, int k )
+pair<int, int> Heap::deleteMin()
 {
-	int min = nodes[ 0 ];
+	int minWeight = weights[ 0 ];
+	int minVertex = vertexes[ 0 ];
 
-	nodes[ 0 ] = nodes[ *n - 1 ];
+	weights[ 0 ] = weights[ n - 1 ];
+	weights.pop_back();
 
-	nodes.pop_back();
+	vertexes[ 0 ] = vertexes[ n - 1 ];
+	vertexes.pop_back();
 
-	*n = *n - 1;
+	n--;
 
-	if ( nodes[ 0 ] > min )
+	if ( weights[ 0 ] > minWeight )
 	{
-		heapifyDown( nodes, *n, 0, k );
+		heapifyDown( 0 );
 	}
 	else
 	{
-		heapifyUp( nodes, *n, k );
+		heapifyUp( n );
 	}
 
-	return min;
+	return make_pair( minWeight, minVertex );
 }
 
 int main()
 {
-	vector<int> nodes;
-	int n = 0;
-	int k = 4;
+	Heap heap( 4 );
 
-	insert( nodes, &n, k, 10 );
-	insert( nodes, &n, k, 18 );
-	insert( nodes, &n, k, 11 );
-	insert( nodes, &n, k, 15 );
-	insert( nodes, &n, k, 12 );
-	insert( nodes, &n, k, 14 );
-	insert( nodes, &n, k, 13 );
-	insert( nodes, &n, k, 99 );
-	insert( nodes, &n, k, 60 );
-	insert( nodes, &n, k, 1 );
-	insert( nodes, &n, k, 2 );
-	insert( nodes, &n, k, 70 );
-	insert( nodes, &n, k, 56 );
-	insert( nodes, &n, k, 33 );
-	insert( nodes, &n, k, 8 );
+	heap.insert( 10, 1 );
+	heap.insert( 18, 2 );
+	heap.insert( 11, 3 );
+	heap.insert( 15, 4 );
+	heap.insert( 12, 5 );
+	heap.insert( 14, 6 );
+	heap.insert( 13, 7 );
+	heap.insert( 99, 8 );
+	heap.insert( 60, 9 );
+	heap.insert( 1, 10 );
+	heap.insert( 2, 11 );
+	heap.insert( 70, 12 );
+	heap.insert( 56, 13 );
+	heap.insert( 33, 14 );
+	heap.insert( 8, 15 );
+	heap.insert( 50, 16 );
 
-	printf( "\n\nHeap after insertion of 8: \n" );
-	for ( int i = 0; i < n; i++ )
-		printf( "%d ", nodes[i] );
+	heap.deleteMin();
+	heap.printWeights();
 
-	printf( "\n\nExtracted min is %d", deleteMin( nodes, &n, k ) );
-	printf( "\n\nHeap after extract min: \n" );
-	for ( int i = 0; i < n; i++ )
-		printf( "%d ", nodes[i] );
+	heap.deleteMin();
+	heap.printWeights();
 
-	printf( "\n\nExtracted min is %d", deleteMin( nodes, &n, k ) );
-	printf( "\n\nHeap after extract min: \n" );
-	for ( int i = 0; i < n; i++ )
-		printf( "%d ", nodes[i] );
+	heap.deleteMin();
+	heap.printWeights();
 
-	insert( nodes, &n, k, 50 );
-
-	printf( "\n\nHeap after insertion of 50: \n" );
-	for ( int i = 0; i < n; i++ )
-		printf( "%d ", nodes[i] );
-
-	printf( "\n\nExtracted min is %d", deleteMin( nodes, &n, k ) );
-	printf( "\n\nHeap after extract min: \n" );
-	for ( int i = 0; i < n; i++ )
-		printf( "%d ", nodes[i] );
-
-	printf( "\n\nExtracted min is %d", deleteMin( nodes, &n, k ) );
-	printf( "\n\nHeap after extract min: \n" );
-	for ( int i = 0; i < n; i++ )
-		printf( "%d ", nodes[i] );
+	heap.deleteMin();
+	heap.printWeights();
 	
-	printf( "\n\nExtracted min is %d", deleteMin( nodes, &n, k ) );
-	printf( "\n\nHeap after extract min: \n" );
-	for ( int i = 0; i < n; i++ )
-		printf( "%d ", nodes[i] );
+	heap.deleteMin();
+	heap.printWeights();
 
-	printf( "\n\nExtracted min is %d", deleteMin( nodes, &n, k ) );
-	printf( "\n\nHeap after extract min: \n" );
-	for ( int i = 0; i < n; i++ )
-		printf( "%d ", nodes[i] );
+	heap.deleteMin();
+	heap.printWeights();
 
 	return 0;
 }
